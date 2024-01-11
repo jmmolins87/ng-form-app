ceb827394a7cd7770bd80fda50b1333b16a75ecd
Juanma <35576283+jmmolins87@users.noreply.github.com>
2024-01-11 17:54:38 +0100

    wip

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { 
   FormArray,
   FormBuilder, 
+  FormControl, 
   FormGroup, 
   Validators 
 } from '@angular/forms';
@@ -21,6 +22,8 @@ export class DynamicPageComponent {
       [ 'Death Stranding', Validators.required ]
     ])
   })
+
+  public newFavorite: FormControl = new FormControl( '', Validators.required );
 
   constructor( private fb: FormBuilder ) {}
 
@@ -61,6 +64,17 @@ export class DynamicPageComponent {
 
   }
 
+  onAddToFavorites(): void {
+    if( this.newFavorite.invalid ) return;
+    const newGame = this.newFavorite.value;
+
+    // Para el form builder
+    // this.favoriteGames.push( new FormControl( newGame, Validators.required ) );
+
+    this.favoriteGames.push( this.fb.control( newGame, Validators.required ));
+    this.newFavorite.reset();
+  }
+
   onDeleteFavorite( index: number ): void { 
     this.favoriteGames.removeAt( index );
   }  
@@ -71,6 +85,7 @@ export class DynamicPageComponent {
       return;
     }
     console.log( this.myForm.value );
+    ( this.myForm.controls['favoriteGames'] as FormArray ) = this.fb.array([]);
     this.myForm.reset();
   }
 
